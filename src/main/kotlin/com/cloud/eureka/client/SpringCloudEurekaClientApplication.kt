a487c1c9fda6b11225a32b2d87bfa285c80c04f3
heehongahn <heehong.ahn@rnrvh.com>
2023-05-16 17:30:48 +0900

    init project

---
+++ src/main/kotlin/com/cloud/eureka/client/SpringCloudEurekaClientApplication.kt
@@ -0,0 +1,13 @@
+package com.cloud.eureka.client
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+
+@SpringBootApplication
+@EnableDiscoveryClient
+class SpringCloudEurekaClientApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringCloudEurekaClientApplication>(*args)
+}
---
+++ src/test/kotlin/com/cloud/eureka/client/SpringCloudEurekaClientApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cloud.eureka.client
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringCloudEurekaClientApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
